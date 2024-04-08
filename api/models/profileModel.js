146fff2ffe967fe0105fcd18673aa5e0699a854a
Alexa Clark <lex.gwc@gmail.com>
2024-04-08 16:56:38 -0400

    Add additional validation to profile schema

--- api/models/profileModel.js
+++ api/models/profileModel.js
@@ -17,7 +17,49 @@ const profileSchema = new mongoose.Schema({
     required: true
   },
   programType: {type: String, required: true},
-  industryInterests: { type: String, required: true },
+  industryInterests: {
+    type: [{
+      type: String,
+      enum: [
+        'Tech',
+        'Healthcare',
+        'Finance',
+        'Education',
+        'Energy',
+        'Real Estate',
+        'Manufacturing',
+        'Consumer Retail',
+        'Entertainment',
+        'Media',
+        'Transportation',
+        'Logistics',
+        'Agriculture',
+        'Food & Beverage',
+        'Hospitality',
+        'Tourism',
+        'Legal',
+        'Environmental',
+        'Biotechnology',
+        'Pharmaceuticals',
+        'Art & Design',
+        'Non-Profit',
+        'Government',
+        'Defense & Space',
+        'Telecommunications',
+        'Publishing',
+        'Fashion',
+        'Sports',
+        'Gaming',
+        'Virtual Reality',
+        'Augmented Reality',
+        'Artificial Intelligence',
+        'Machine Learning',
+        'Blockchain',
+        'Cryptocurrency',
+        'Other'
+      ]
+    }],
+    required: true },
   isTechnical: { type: Boolean, required: true },
   linkedinURL: { type: String, required: false },
   email: { type: String, required: true },
@@ -27,10 +69,37 @@ const profileSchema = new mongoose.Schema({
   employmentHistory: { type: String, required: true },
   gender: { type: String, required: false },
   schedulingURL: { type: String, required: false },
-  hasIdea: { type: String, required: true },
+  hasIdea: {
+    type: String,
+    enum: [
+      "Committed to an idea",
+      "Have ideas but not committed",
+      "Don't have an idea yet"
+    ],
+    required: true
+  },
   ideasInterestedIn: { type: String, required: false },
-  areasOfResponsibility: { type: String, required: true },
-  timelineForFulltime: { type: String, required: true },
+  areasOfResponsibility: {
+    type: [{ type: String, enum: [
+      'Business Strategy',
+      'Product Development',
+      'Marketing and Sales',
+      'Technology and Engineering',
+      'Operations',
+      'Finance and Fundraising',
+      'Legal and Compliance'
+    ]}],
+    required: true},
+    timelineForFulltime: {
+      type: String,
+      enum: [
+        'Immediately', 
+        'Within 6 Months',
+        'Within a Year', 
+        'More than a Year', 
+        'Undecided'
+      ],
+      required: true },
   cofounderDesiredQualities: { type: String, required: false },
   impressiveAccomplishment: { type: String, required: false },
   lastSeen: { type: Date, required: true }
